0bae6bf2b946567f9df7dd9e79e86936167df296
Nathaniel C Griggs <88065063+cayscorruptedmind@users.noreply.github.com>
2021-11-05 11:26:27 -0400

    Create calulator.py

---
+++ calulator.py
@@ -0,0 +1,9 @@
+# Building a Calculator
+print  ('Welcome to my simple Multiplication calculator')
+
+num_1 = float(input("enter first number: "))
+num_2 = float(input("enter second number: "))
+
+answer = num_1 * num_2
+
+print (answer)
